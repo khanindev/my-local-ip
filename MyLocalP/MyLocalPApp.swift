c7db5c4089172c4028004864fb5a04efd853f02f
Dmitriy Khanin <dmkhaninyt@yandex.ru>
2023-10-24 01:27:31 +0300

    move app to popover, and add quit from app

--- MyLocalP/MyLocalPApp.swift
+++ MyLocalP/MyLocalPApp.swift
@@ -6,13 +6,77 @@
 //
 
 import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var menu: NSMenu!
+    var popover: NSPopover!
+    
+    var statusBarItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        // Close others window`s
+        if let window = NSApplication.shared.windows.first {
+            window.close()
+        }
+        
+        // Build popover
+        let mainView = MainView()
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 300, height: 200)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: mainView)
+        self.popover = popover
+        
+        // Build menu
+        let statusBarMenu = NSMenu(title: "Actions")
+        statusBarMenu.addItem(
+            withTitle: "Close application",
+            action: #selector(closeApp),
+            keyEquivalent: ""
+        )
+        
+        self.menu = statusBarMenu;
+        
+        // Build status bar item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(systemSymbolName: "globe", accessibilityDescription: "Globe icon")
+            
+            // Set status button actions
+            button.action = #selector(statusItemClick(sender:))
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        }
+         
+    }
+    
+    @objc func statusItemClick(sender: NSStatusItem) {
+        let event = NSApp.currentEvent!
+        let button = self.statusBarItem.button!
+        
+        if event.type == NSEvent.EventType.rightMouseUp {
+            NSMenu.popUpContextMenu(self.menu!, with: event, for: button)
+        } else {
+            self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+    
+    @objc func closeApp() {
+        NSApplication.shared.terminate(self)
+    }
+}
 
 @main
 struct MyLocalPApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
-        MenuBarExtra("Current IP Address", systemImage: "globe") {
-            MainView()
-        }.menuBarExtraStyle(.window)
-        WindowGroup {}
+        WindowGroup {
+            ZStack {
+                 EmptyView()
+               }
+               .hidden()
+        }
     }
 }
--- MyLocalP/ViewModels/ContentViewVM.swift
+++ MyLocalP/ViewModels/ContentViewVM.swift
@@ -6,11 +6,16 @@
 //
 
 import Foundation
+import AppKit
 
 class ContentViewVM: ObservableObject {
     let ipAddress: String = getIPAddress()
     
     func handleCopyClick() -> Void {
         copyToClipboard(text: self.ipAddress)
+    }
+    
+    func handleCloseClick() -> Void {
+        NSApplication.shared.terminate(self)
     }
 }
